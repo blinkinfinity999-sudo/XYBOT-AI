e257eb2df551140e495776b9262704d504dc1b0a
blinkinfinity999-sudo <blinkinfinity999@gmail.com>
2026-08-29 19:08:06 +0530

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAIcTOcZTc5i-_JiB-MbRakQtT93n7H7TQ",
+  authDomain: "xybot-ai.firebaseapp.com",
+  projectId: "xybot-ai",
+  storageBucket: "xybot-ai.firebasestorage.app",
+  messagingSenderId: "700172856791",
+  appId: "1:700172856791:web:d57ae0fc7a4612a486af92"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
